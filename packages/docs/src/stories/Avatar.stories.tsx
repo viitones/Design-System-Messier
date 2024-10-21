9eb5ffc25cced84dbb59cf4c1c13fc025e0e5c95
viitones <vitonesmartins@gmail.com>
2024-10-21 08:18:11 -0300

    Avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import { Avatar, AvatarProps } from '@messier-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/viitones.png',
+    alt: 'Victor Hugo',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
