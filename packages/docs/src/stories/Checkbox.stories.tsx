0ed242ce6151162cc82866e6351848dd0cbf6e6a
viitones <vitonesmartins@gmail.com>
2024-10-23 22:45:14 -0300

    checkbox

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,21 @@
+import { Box, Checkbox, CheckboxProps, Text } from '@messier-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+export const Primary: StoryObj<CheckboxProps> = {}
