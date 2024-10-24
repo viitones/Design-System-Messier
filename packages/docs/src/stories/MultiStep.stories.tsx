2cad3cad2580320d768bb8217f1096ad3e9783c2
viitones <vitonesmartins@gmail.com>
2024-10-24 07:33:01 -0300

    current step

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,31 @@
+import { Box, MultiStep, MultiStepProps } from '@messier-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/Multi Step',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 2,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {}
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4,
+  },
+}
